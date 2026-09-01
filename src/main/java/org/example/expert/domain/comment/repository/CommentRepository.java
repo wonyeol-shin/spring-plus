03982f48ab3068b714dca874d8823087e924dccc
wonyeolShin <lwy171400@gmail.com>
2026-09-01 12:48:26 +0900

    [level2] N+1 문제 해결

--- src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
+++ src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
@@ -9,6 +9,11 @@ import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    @Query("SELECT c FROM Comment c JOIN c.user WHERE c.todo.id = :todoId")
+    @Query("""
+    SELECT c
+    FROM Comment c
+    LEFT JOIN FETCH c.user u
+    WHERE c.todo.id = :todoId
+""")
     List<Comment> findByTodoIdWithUser(@Param("todoId") Long todoId);
 }
