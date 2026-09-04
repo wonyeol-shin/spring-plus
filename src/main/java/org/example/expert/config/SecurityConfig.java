235e3b3b122b1df6fb91d3851b9e8e2339dd827b
wonyeolShin <lwy171400@gmail.com>
2026-09-04 12:35:25 +0900

    [level3] websocket 허용

--- src/main/java/org/example/expert/config/SecurityConfig.java
+++ src/main/java/org/example/expert/config/SecurityConfig.java
@@ -27,6 +27,7 @@ public class SecurityConfig {
                 .addFilterBefore(jwtFilter, SecurityContextHolderAwareRequestFilter.class)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/ws/**").permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 ).build();
